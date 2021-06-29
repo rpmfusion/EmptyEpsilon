92a014967c5544fd65be2ddf5826afae3f544e4b
Michal Schorm <mschorm@redhat.com>
2021-06-29 15:15:25 +0200

    Fix the Findjson11.cmake to avoid the CMake warning about case sensitivity (json11 X JSON11)

--- Findjson11.cmake
+++ Findjson11.cmake
@@ -1,14 +1,14 @@
 # Locate json11
 #
 # This module defines
-#  JSON11_FOUND, if false, do not try to link to json11
-#  JSON11_LIBRARY, where to find json11
-#  JSON11_INCLUDE_DIR, where to find json11.hpp
+#  json11_FOUND, if false, do not try to link to json11
+#  json11_LIBRARY, where to find json11
+#  json11_INCLUDE_DIR, where to find json11.hpp
 #
-# If json11 is not installed in a standard path, you can use the JSON11_DIR CMake variable
+# If json11 is not installed in a standard path, you can use the json11_DIR CMake variable
 # to tell CMake where json11 is.
 
-find_path(JSON11_INCLUDE_DIR json11.hpp
+find_path(json11_INCLUDE_DIR json11.hpp
           PATHS
           /usr/local/include/
           /usr/include/
@@ -22,7 +22,7 @@ if(NOT WIN32)
     SET(CMAKE_FIND_LIBRARY_SUFFIXES ".so" ".a")
 endif(NOT WIN32)
 
-find_library(JSON11_LIBRARY
+find_library(json11_LIBRARY
              NAMES  json11
              PATH_SUFFIXES lib64 lib
              PATHS  /usr/local
@@ -33,7 +33,7 @@ find_library(JSON11_LIBRARY
                     /opt
                     /lib)
 
-# handle the QUIETLY and REQUIRED arguments and set JSON11_FOUND to TRUE if all listed variables are TRUE
+# handle the QUIETLY and REQUIRED arguments and set json11_FOUND to TRUE if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(JSON11 DEFAULT_MSG JSON11_INCLUDE_DIR JSON11_LIBRARY)
-mark_as_advanced(JSON11_INCLUDE_DIR JSON11_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(json11 DEFAULT_MSG json11_INCLUDE_DIR json11_LIBRARY)
+mark_as_advanced(json11_INCLUDE_DIR json11_LIBRARY)
